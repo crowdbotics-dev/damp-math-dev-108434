6575e5b8213d25d7b595eb9fbcaa33764cb89679
Crowdbotics <engineering@crowdbotics.com>
2023-08-11 07:34:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "damp-math-dev-108434",
+  url: "https://damp-math-dev-108434.botics.co",
+  api: "https://damp-math-dev-108434.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
